494d33a03b47ee6c68fda7d84892e8bcd5bc5a85
branzoo77 <48650248+branzoo77@users.noreply.github.com>
2023-01-06 10:43:17 +0100

    Added much nicer reading of the input data

--- data_transformer.py
+++ data_transformer.py
@@ -1,4 +1,4 @@
-import re
+import numpy as np
 
 width_in_mm = 14.7
 height_in_mm = 9.8
@@ -6,28 +6,19 @@ depth_in_mm = 8.575
 
 voxel_size = 0.02449999935925007
 
-org = open('data/cochlea_spiral_points.txt', 'r')
 trn = open('data/cochlea_spiral_points_transformed.txt', 'w')
 
-while 1:
-    line_in = org.readline()
-    if line_in == '':
-        break
-    else:
-        numbers = re.findall(r'-?\d+.\d+', line_in)
-        trn.write(str(round(((float(numbers[0])) + width_in_mm/2)/voxel_size)))
-        trn.write(' ')
-        trn.write(str(round(((float(numbers[1])) + height_in_mm/2)/voxel_size)))
-        trn.write(' ')
-        trn.write(str(round(((float(numbers[2])) + depth_in_mm/2)/voxel_size)))
-        trn.write('\n')
+data = np.genfromtxt('data/cochlea_spiral_points.txt', dtype='float')
+x = data[:, 0]
+y = data[:, 1]
+z = data[:, 2]
+
+for i in range(0, x.size):
+    trn.write(str(round((x[i] + width_in_mm / 2) / voxel_size)))
+    trn.write(' ')
+    trn.write(str(round((y[i] + height_in_mm / 2) / voxel_size)))
+    trn.write(' ')
+    trn.write(str(round((z[i] + depth_in_mm / 2) / voxel_size)))
+    trn.write('\n')
 
 trn.close()
-org.close()
-
-
-
-
-
-
-
