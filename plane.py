803bae5a4795b1048bb3be3545d12756cf44861b
AleksandarLukic96 <60717693+AleksandarLukic96@users.noreply.github.com>
2023-01-09 12:16:11 +0100

    Added set_axis and adjusted linespaces

--- plane.py
+++ plane.py
@@ -4,16 +4,22 @@ import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-x = np.linspace(-10, 10, 100)
-y = np.linspace(-10, 10, 100)
+x = np.linspace(-3, 3, 3)
+y = np.linspace(-3, 3, 3)
 
 x, y = np.meshgrid(x, y)
-eq = 0.12 * x + 0.01 * y + 1.09
+
+plane_equation = 0.12 * x + 0.01 * y + 1.09
 
 fig = plt.figure()
 
 ax = plt.axes(projection='3d')
 
-ax.plot_surface(x, y, eq, color='r')
+ax.plot_surface(x, y, plane_equation, color='red')
+
+ax.set_xlim(-10, 10) 
+ax.set_ylim(-10, 10) 
+ax.set_zlim(-10, 10)
+
 
 plt.show()
