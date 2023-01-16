154afe668e88b074eca069dd2c2eb379fa364b2a
Freja Slot Hansen <s204493@student.dtu.dk>
2023-01-16 11:58:09 +0100

    Tidying up in various files

--- data_transformer.py
+++ data_transformer.py
@@ -1,29 +1,27 @@
 import os
 import numpy as np
 
-width_in_mm = 14.7
-height_in_mm = 9.8
-depth_in_mm = 8.575
+# # Define path to directory on any machine
+# path = os.getcwd() + "\\data\\"
+#
+# datapoints = np.genfromtxt('data/cochlea_spiral_points.txt', dtype='float')
 
-voxel_size = 0.02449999935925007
 
-# Define path to directory on any machine
-path = os.getcwd() + "\\data\\"
-#print("path_to_nii: " + path_to_nii)
+# Transform data points in physical space to voxels
+def transform_data(data, img):
+    trn = open('data/cochlea_spiral_points_transformed.txt', 'w')
 
-trn = open(path + "cochlea_spiral_points_transformed.txt", 'w')
+    voxel_size = img.header['pixdim'][1]
+    x = data[:, 0]
+    y = data[:, 1]
+    z = data[:, 2]
 
-data = np.genfromtxt('data/cochlea_spiral_points.txt', dtype='float')
-x = data[:, 0]
-y = data[:, 1]
-z = data[:, 2]
+    for i in range(0, x.size):
+        trn.write(str(round((x[i] + img.header['qoffset_x']) / voxel_size)))
+        trn.write(' ')
+        trn.write(str(round((y[i] + img.header['qoffset_y']) / voxel_size)))
+        trn.write(' ')
+        trn.write(str(round((z[i] - img.header['qoffset_z']) / voxel_size)))
+        trn.write('\n')
 
-for i in range(0, x.size):
-    trn.write(str(round((x[i] + width_in_mm / 2) / voxel_size)))
-    trn.write(' ')
-    trn.write(str(round((y[i] + height_in_mm / 2) / voxel_size)))
-    trn.write(' ')
-    trn.write(str(round((z[i] + depth_in_mm / 2) / voxel_size)))
-    trn.write('\n')
-
-trn.close()
+    trn.close()
