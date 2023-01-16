64c9aa7e4c10f408ea3078efed38c649aa4f1269
VictoriaIpsen <victoriai@live.dk>
2023-01-16 16:40:27 +0100

    Files for interpolation points and its normal vectors, and interpolation of voxel values

--- normalVector3points.py
+++ normalVector3points.py
@@ -3,7 +3,7 @@ np.set_printoptions(precision=7, suppress=True)
 import nibabel as nib
 from matplotlib import pyplot as plt
 
-
+Path = 'C:\\Users\\victo\\02507_Project_ImageAnalysis_ComputerGraphics\\'
 path = 'C:\\Users\\victo\\Dropbox\\Uni\\DTU\\MMC\\Project Image Analysis\\cochlea_spiral_points.txt'
 pathImg = 'C:\\Users\\victo\\Dropbox\\Uni\\DTU\\MMC\\Project Image Analysis\\cochlea.nii'
 pathSeg = 'C:\\Users\\victo\\Dropbox\\Uni\\DTU\\MMC\\Project Image Analysis\\cochlea_segmentation.nii'
@@ -14,21 +14,15 @@ data = img.get_fdata()
 imgSegmentation = nib.load(pathSeg)
 seg = imgSegmentation.get_fdata()
 
-# Displaying slice
-plt.title("Slice")
-plt.xlabel("width pixel scaling")
-plt.ylabel("height pixels scaling")
-
-plt.imshow(data[:,:,2],cmap='gray')
-plt.show()
-
 # Making points from physical coordinates to voxel coordinates
-pointsMm = np.loadtxt(path)
-#voxelSize = 0.02449999935925007
-voxelSize = img.header.get_zooms()[0] #mm
-correction = np.ones(pointsMm.shape)*np.divide(img.shape,2)
-pointsVoxel = np.divide(pointsMm, voxelSize) + correction
-#roundPointsVoxel = np.around(np.divide(pointsMm, voxelSize) + correction)
+# pointsMm = np.loadtxt(path)
+# voxelSize = img.header.get_zooms()[0] #mm
+# correction = np.ones(pointsMm.shape)*np.divide(img.shape,2)
+# pointsVoxel = np.divide(pointsMm, voxelSize) + correction
+# roundPointsVoxel = np.around(np.divide(pointsMm, voxelSize) + correction)
+
+# Interpolated points in voxels
+pointsVoxel = np.loadtxt(Path + 'interpolatedPoints.txt')
 
 # Finding circle center and radius from 3 points
 # https://itecnote.com/tecnote/python-find-arc-circle-equation-given-three-points-in-space-3d/
@@ -43,9 +37,17 @@ for n, point in enumerate(pointsVoxel):
     p2 = pointsVoxel[idx, :]
     p3 = pointsVoxel[idx+1, :]
 
-    a = np.linalg.norm(p3 - p2)
-    b = np.linalg.norm(p3 - p1)
-    c = np.linalg.norm(p2 - p1)
+    p1p2 = np.subtract(p2, p1)
+    p1p3 = np.subtract(p3, p1)
+    p2p3 = np.subtract(p3, p2)
+
+    if np.all(np.cross(p1p2, p1p3) <= 3.0e-12):
+        normalVec[idx, :] = p1p2
+        continue
+
+    a = np.linalg.norm(p2p3)
+    b = np.linalg.norm(p1p3)
+    c = np.linalg.norm(p1p2)
 
     s = (a + b + c) / 2
     R = a * b * c / 4 / np.sqrt(s * (s - a) * (s - b) * (s - c))
@@ -55,12 +57,19 @@ for n, point in enumerate(pointsVoxel):
     center = np.column_stack((p1, p2, p3)).dot(np.hstack((b1, b2, b3)))
     center /= b1 + b2 + b3
 
-    p1p2 = np.subtract(p2, p1)
-    p1p3 = np.subtract(p3, p1)
     p2c = np.subtract(center, p2)
     n = np.cross(p1p2, p1p3)
     nVec = np.cross(p2c, n)
     normalVec[idx,:] = nVec / np.linalg.norm(nVec) * 20
+
+normalVec[0,:] = np.subtract(pointsVoxel[1,:],pointsVoxel[0,:])# first normal vector
+normalVec[normalVec.shape[0]-1,:] = normalVec[(normalVec.shape[0]-2),:]# last normal vector
+
+# Saving txt file with normal vectors
+normalVecMatrix = np.matrix(normalVec)
+with open(Path + 'normalVec.txt', 'wb') as f:
+    for line in normalVecMatrix:
+        np.savetxt(f, line, fmt='%.2f')
 
 normalVecPlot = np.hstack((pointsVoxel,normalVec))
 
@@ -71,8 +80,6 @@ ax = plt.axes(projection="3d")
 
 # Creating plot
 ax.scatter3D(pointsVoxel[:,0], pointsVoxel[:,1], pointsVoxel[:,2], color="green")
-ax.scatter3D(pointsVoxel[0,0], pointsVoxel[0,1], pointsVoxel[0,2], color="red")
-ax.scatter3D(center[0], center[1], center[2], color="red")
 ax.quiver(normalVecPlot[1:(normalVecPlot.shape[0]-1),0],normalVecPlot[1:(normalVecPlot.shape[0]-1),1],
           normalVecPlot[1:(normalVecPlot.shape[0]-1),2],normalVecPlot[1:(normalVecPlot.shape[0]-1),3],
           normalVecPlot[1:(normalVecPlot.shape[0]-1),4],normalVecPlot[1:(normalVecPlot.shape[0]-1),5])
