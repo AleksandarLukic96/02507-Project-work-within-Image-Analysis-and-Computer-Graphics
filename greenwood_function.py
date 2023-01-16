4476d8c06fd5dc5918fc47717cb27eb0d05304aa
Caroline <carlankjaer@gmail.com>
2023-01-16 13:12:29 +0100

    Updated the documentation for the greenwoodFunction and extended with a function to get frequency for a specific point

--- greenwood_function.py
+++ greenwood_function.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-# Function to calculate the frequency that can be heard 
-# at certain points in the cochlea according to its length
-def greenwoodFunction(cochlealength):
+def greenwoodFunction(cochlealength) -> list:
+    """Returns a list of frequencies that can be heard 
+    at certain points in the cochlea according to its length"""
+    
     # Number of points to find the frequency for, with a distance of
     # 25 microns between the points on the cochlea
     numPoints = round(cochlealength * 1000 / 25)
@@ -14,3 +15,10 @@ def greenwoodFunction(cochlealength):
         frequencies.append(frequency)
     
     return frequencies
+
+def greenwoodFunctionPoint(cochlealength, point) -> float:
+    """Returns the frequency of a specific point in the cochlea.
+    The point is in mm along the length of the cochlea"""
+    x = (cochlealength - point) / cochlealength
+    frequency = 165.4 * (10 ** (2.1 * x) - 0.88)
+    return frequency
