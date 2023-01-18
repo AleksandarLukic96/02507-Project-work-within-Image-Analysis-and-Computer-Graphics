d94e61dde33a56001318664135d1e6ebc39929f3
Caroline <carlankjaer@gmail.com>
2023-01-18 15:20:07 +0100

    Created colored 3D model of frequencies heard in the cochlea

--- greenwood_function.py
+++ greenwood_function.py
@@ -1,22 +1,39 @@
 import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
 
-def greenwoodFunction(cochlealength: float) -> list:
-    """Returns a list of frequencies that can be heard 
-    at certain points in the cochlea according to its length"""
-    
-    # Number of points to find the frequency for, with a distance of
-    # 25 microns between the points on the cochlea
-    numPoints = round(cochlealength * 1000 / 25)
-    
-    frequencies = []
-    for point in np.linspace(0, cochlealength, numPoints, endpoint=True):
-        x = (cochlealength - point) / cochlealength
-        frequency = 165.4 * (10 ** (2.1 * x) - 0.88)
-        frequencies.append(frequency)
-    
-    return frequencies
+pathImgInterpolated = 'data/test_nifty3.nii'
+pathImg = 'data/cochlea.nii'
 
-def greenwoodFunctionPoint(cochlealength: float, point: float) -> float:
+img = nib.load(pathImg)
+imgInterpolated = nib.load(pathImgInterpolated)
+
+img_data = img.get_fdata()
+img_data_interpolated = imgInterpolated.get_fdata()
+
+# Get voxel size from the header of the original image
+voxelSize = img.header.get_zooms()[0]
+
+# Get dimensions of the interpolated .nii image
+imgShape = img_data_interpolated.shape
+
+# Get the length of the cochlea in mm
+cochleaLength = imgShape[0] * voxelSize
+
+def getListOfDataPoints() -> list:
+    """Returns a list of points in the cochlea in mm"""
+    # Number of points found in the cochlea, with a distance of voxel size
+    # between the points on the cochlea
+    numberOfPoints = int(cochleaLength / voxelSize)
+    
+    # List of points in the cochlea
+    points = []
+    for i in range(numberOfPoints + 1):
+        points.append(i * voxelSize)
+    
+    return points
+
+def greenwoodFunctionForPoint(cochlealength: float, point: float) -> float:
     """Returns the frequency of a specific point in the cochlea.
     The point is in mm along the length of the cochlea"""
     x = (cochlealength - point) / cochlealength
@@ -38,23 +55,56 @@ def greenwoodFunctionFromPoints(cochlealength: float, points: list) -> list:
 # Generate color list from frequencies
 def getFrequencyColors(frequencies: list):
     colors = []
-    for i in range(len(frequencies)):
-        if frequencies[i] < 125.0:
+    for frequency in frequencies:
+        if frequency < 125.0:
             colors.append('red')
-        elif frequencies[i] < 250.0:
+        elif frequency < 250.0:
             colors.append('orange')
-        elif frequencies[i] < 500.0:
+        elif frequency < 500.0:
             colors.append('yellow')
-        elif frequencies[i] < 1000.0:
+        elif frequency < 1000.0:
             colors.append('green')
-        elif frequencies[i] < 2000.0:
+        elif frequency < 2000.0:
             colors.append('lime')
-        elif frequencies[i] < 4000.0:
+        elif frequency < 4000.0:
             colors.append('lightblue')
-        elif frequencies[i] < 8000.0:
+        elif frequency < 8000.0:
             colors.append('blue')
-        elif frequencies[i] < 16000.0:
+        elif frequency < 16000.0:
             colors.append('violet')
         else:
             colors.append('purple')
     return colors
+
+# Get the list of data points in mm from the base of the cochlea
+dataPoints = getListOfDataPoints()
+
+# Get list of frequencies
+frequencyList = greenwoodFunctionFromPoints(cochleaLength, dataPoints)
+
+# Get colors for frequencies
+colors = getFrequencyColors(frequencyList)
+
+# Data points to plot
+data = np.genfromtxt('interpolatedPoints.txt', dtype = 'float')
+x = data[:, 0]
+y = data[:, 1]
+z = data[:, 2]
+
+# Displaying points
+# Creating figure
+fig = plt.figure(figsize=(10, 7))
+
+# Set Axes accordingly to img dimensions
+ax = plt.axes(projection="3d")
+
+# Creating plot of spiral points
+ax.scatter3D(x, y, z, c=colors)
+
+plt.title("Spiral points in cochlea")
+plt.xlabel("width")     # x-axis = scanner-left/right 
+plt.ylabel("height")    # y-axis = scanner-floor/ceiling axis
+                        # z-axis = scanner-bore axis
+                        
+# show plot
+plt.show()
